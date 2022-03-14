d509d1d4bb54db45de376b1caa7e248ab0e1c594
charlotte-2222 <ayy.charlotte@gmail.com>
2022-03-13 22:05:08 -0400

    Fixed the main form's code (Ln's 63 - 92) TODO: frmMain visuals, tooltips, tabs, additionals | Finalize comments in code.

--- frmMain.vb
+++ frmMain.vb
@@ -8,6 +8,9 @@ Option Strict On
 Option Explicit On
 Option Infer Off
 Public Class frmMain
+
+
+
     Private Declare Function HideCaret Lib "user32.dll" (ByVal hWnd As IntPtr) As Boolean
     ' ^Hides I-Beam caret on disabled caret.
     Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
@@ -39,47 +42,56 @@ Public Class frmMain
     '-Begin Main Operations-'
     Dim FileInfo(14, 1) As String
     Dim intStudentIndex As Integer
+    Dim Message As DialogResult
 
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.LstGrade.SelectedIndex = 0
         Dim LoadFile As IO.StreamReader 'Stream reader to open coded file
         Dim FailFile As OpenFileDialog = New OpenFileDialog 'catch/backup for failure in stream reader, will allow user to open from Dir
 
-        If IO.File.Exists("NamesAndGraddes.txt") Then
+
+        If IO.File.Exists("NamesAndGradles.txt") Then
             'If it exists in the debug file / is readable, it will open
-            LoadFile = IO.File.OpenText("NamesAndGraddes.txt")
+            LoadFile = IO.File.OpenText("NamesAndGradles.txt")
             Do Until LoadFile.Peek = -1
                 FileInfo(intStudentIndex, 0) = LoadFile.ReadLine
                 FileInfo(intStudentIndex, 1) = LoadFile.ReadLine
                 intStudentIndex += 1
-
             Loop
             LoadFile.Close()
+
+
         Else
-            'If the specified file, in our case "NamesAndGrades.txt", is not present/readable -
-            'the user will be tasked with looking for it themselves
-            MessageBox.Show("Unable to open the intended file - it was either missing from the directory or is not readable. Please select a file from the directory.",
-            "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error)
-            If FailFile.ShowDialog() = DialogResult.OK Then
-                LoadFile = IO.File.OpenText(FailFile.FileName)
-                Do Until LoadFile.Peek = -1
-                    FileInfo(intStudentIndex, 0) = LoadFile.ReadLine
-                    FileInfo(intStudentIndex, 1) = LoadFile.ReadLine
-                    intStudentIndex += 1
-
-                Loop
-                LoadFile.Close()
-                If FailFile.ShowDialog() = DialogResult.Cancel Then
-                    'Ok/Cancel options to close the application
-                    ' !! Does not work as intended - has unintentional effects. Will revisit. !!
 
 
+            Message = MessageBox.Show("Unable to open the intended file. It was either missing or unreadable. Please select a file from the directory, or cancel to exit.",
+                            "Error",
+                            MessageBoxButtons.OKCancel,
+                            MessageBoxIcon.Error)
 
-                Else
-                    Me.Close()
+
+            If Message = System.Windows.Forms.DialogResult.Cancel Then
+                Me.Close()
+            Else
+                If Message = System.Windows.Forms.DialogResult.OK Then
+
+
+                    If FailFile.ShowDialog() = DialogResult.OK Then
+                        LoadFile = IO.File.OpenText(FailFile.FileName)
+                        Do Until LoadFile.Peek = -1
+                            FileInfo(intStudentIndex, 0) = LoadFile.ReadLine
+                            FileInfo(intStudentIndex, 1) = LoadFile.ReadLine
+                            intStudentIndex += 1
+                        Loop
+                        LoadFile.Close()
+                    End If
                 End If
+
             End If
+
         End If
+
+
     End Sub
 
     Private Sub BtnDisplay_Click(sender As Object, e As EventArgs) Handles BtnDisplay.Click
